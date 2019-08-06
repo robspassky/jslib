31a03bec19a4d85d1f55139df1011fbaaf0224fe
Rob Cabacungan <robspassky@gmail.com>
2019-08-05 22:26:04 -0400

    feat(sll): implement node-based sll

--- src/SLL.js
+++ src/SLL.js
@@ -1,24 +1,53 @@
-/** Class that implements a singly-linked list */
-class SLL {
-  constructor() {
-    this.theList = []
-  }
+/**
+ * Singly-linked list module
+ * @module {Class} SLL
+ */
 
-  /** 
-   * Append an object to end of list
-   * @param {any} x - object to append
+/**
+ * Class representing a single node of an SLL
+ * @private
+ */
+class Node {
+  /**
+   * constructs a node
+   * @param {any} datum - the data to store in the node
+   * @param {Node} pnext - the next node in the list
    */
-  add(x) {
-    this.theList.push(x)
-  }
-
-  /** 
-   * Return current size of list 
-   * @returns {number} size of list
-   */
-  size() {
-    return this.theList.length
+  constructor (datum, pnext) {
+    this.datum = datum
+    this.pnext = pnext
   }
 }
 
-module.exports = SLL
+/** Class that implements a singly-linked list */
+module.exports = class SLL {
+  constructor () {
+    this.theList = new Node(undefined, undefined)
+  }
+
+  /**
+   * Append an object to end of list
+   * @param {any} x - object to append
+   */
+  add (x) {
+    let p = this.theList
+    while (p.pnext !== undefined) {
+      p = p.pnext
+    }
+    p.pnext = new Node(x, undefined)
+  }
+
+  /**
+   * Return current size of list
+   * @returns {number} size of list
+   */
+  size () {
+    let size = 0
+    let p = this.theList
+    while (p.pnext !== undefined) {
+      p = p.pnext
+      size += 1
+    }
+    return size
+  }
+}
